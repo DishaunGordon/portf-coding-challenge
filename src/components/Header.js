fe63193753e23d0635062824e613d68b44ca6b65
DishaunGordon <dishaun5@gmail.com>
2022-07-24 12:22:53 +0100

    Including "date" and "abv" in div

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ function Header() {
     return ( 
         <div className="header">
             <div className="header__date">
-                <div className="header__date--title"></div>
+                <div className="header__date--title">Date</div>
                 <div className="header__date--input"></div>
             </div>
             <div className="header__abv">
-                <div className="header__abv--title"></div>
+                <div className="header__abv--title">abv</div>
                 <div className="header__abv--input"></div>
             </div>
         </div>
