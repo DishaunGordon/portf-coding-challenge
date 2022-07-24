5cfd856128911fdd390f557c72338e3a821ec825
DishaunGordon <dishaun5@gmail.com>
2022-07-24 12:20:36 +0100

    Creating Graph component

---
+++ src/components/Graph.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Graph(data) {
+    console.log("rendering and data is -" + JSON.stringify(data?.[0]))
+    return ( 
+        <div>
+            
+        </div>
+    )
+}
+
+export default Graph;
