040123bfaae2413938c12868db4df20612574976
DishaunGordon <dishaun5@gmail.com>
2022-07-24 13:38:39 +0100

    Inserted datepicker, passed state to Header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
+import React, { useState } from 'react';
+
 import './App.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 import UseAxios from './hooks/useAxios';
 import Header from './components/Header';
 import Graph from './components/Graph';
 
 function App() {
-  const {data, isLoading, error} = UseAxios("https://api.punkapi.com/v2/beers?per_page=80")
+  const {data, isLoading, error} = UseAxios("https://api.punkapi.com/v2/beers?per_page=80");
+  const {startDate, setStartDate} = useState(new Date("April, 2007"));
+  const {endDate, setEndDate} = useState(new Date());
+  const {abvFilter, setAbvFilter} = useState(10);
+
+  const handleSetStartDate = (newStartDate) => {
+    setStartDate(newStartDate);
+  }
+  const handleSetEndDate = (newEndDate) => {
+    setEndDate(newEndDate);
+  }
+  const handleSetAbvFilter = (newAbvFilter) => {
+    setAbvFilter(newAbvFilter);
+  }
    
   return (
     <div className="container">
-      <Header />
+      <Header 
+        StartDate={startDate} handleSetStartDate={handleSetStartDate}
+        EndDate={endDate} handleSetEndDate={handleSetEndDate}
+        AbvFilter={abvFilter} handleSetAbvFilter={handleSetAbvFilter}
+      />
       <Graph data={data} />
     </div>
   )
 }
 
 export default App;
+
+//TODO -- Fix - Says setStartDate isn't a function when datepicker in <Header /> tries to update it 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,44 @@
 import React from 'react';
-function Header() {
+
+import DatePicker from 'react-datepicker';
+
+function Header({ startDate, handleSetStartDate, endDate, handleSetEndDate, abvFilter, handleSetAbvFilter }) {
+    const onStartDateChange = (newDate) => {
+        handleSetStartDate(newDate);
+    };
+    const onEndDateChange = (newDate) => {
+        handleSetEndDate(newDate);
+    };
+    const onAbvFilterChange = (e) => {
+        handleSetAbvFilter(e.target.value);
+    };
+
     return ( 
         <div className="header">
             <div className="header__date">
                 <div className="header__date--title">Date</div>
-                <div className="header__date--input"></div>
+                <div className="header__date--inputContainer">
+                <DatePicker className="header__date--pickerStart"
+                    selected={startDate}
+                    onChange={onStartDateChange}
+                    dateFormat="MM/yyyy"
+                    showMonthYearPicker
+                    showFullMonthYearPicker
+                />
+                <DatePicker className="header__date--pickerEnd"
+                    selected={endDate}
+                    onChange={onEndDateChange}
+                    dateFormat="MM/yyyy"
+                    showMonthYearPicker
+                    showFullMonthYearPicker
+                />
+                </div>
             </div>
             <div className="header__abv">
                 <div className="header__abv--title">abv</div>
-                <div className="header__abv--input"></div>
+                <div className="header__abv--inputContainer">
+
+                </div>
             </div>
         </div>
     );
