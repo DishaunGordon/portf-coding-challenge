c01888845c29d483445d066381dda61550717092
DishaunGordon <dishaun5@gmail.com>
2022-07-25 13:39:34 +0100

    return info from graph for debugging

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,11 +1,17 @@
 import React from 'react';
-function Graph(data) {
-    console.log("rendering and data is -" + JSON.stringify(data?.[0]))
+function Graph({ data }) {
     return ( 
         <div>
-            
+            <ul>
+                {console.log(data)}
+                {data.map(beer => {
+                    return(
+                    <li key={beer.id}>Name is {beer.name}</li>
+                    )
+                })}
+            </ul>
         </div>
-    )
+     );
 }
 
 export default Graph;
