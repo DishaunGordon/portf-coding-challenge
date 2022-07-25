638e72a59bf0232084d195e89ba7cacc7734724e
DishaunGordon <dishaun5@gmail.com>
2022-07-25 13:38:12 +0100

    MainBody contains logic of what to render

---
+++ src/components/MainBody.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BallTriangle } from  'react-loader-spinner'
+
+import ErrorHandler from './ErrorHandler';
+import Graph from './Graph';
+
+
+function MainBody({ data, isLoading, error }) {
+
+    const handleRender = () => {
+        if (error) return <ErrorHandler error={error} />
+        if (isLoading) return <BallTriangle />
+        return <Graph data={data} />
+    }
+
+    return (
+        <div className="mainBody">
+            {handleRender()}
+        </div>
+    )
+}
+
+export default MainBody;
