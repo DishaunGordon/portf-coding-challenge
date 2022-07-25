fe9acb1b421e5c048ac09f093c2de7ef49fcf059
DishaunGordon <dishaun5@gmail.com>
2022-07-25 10:35:12 +0100

    fixing loop, adding comments, removing debugg code

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -3,15 +3,17 @@ import axios from 'axios'
 
 
 function UseAxios(receivedUrl) {
+    // States
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [isAllDataCollected, setIsAllDataCollected] = useState(false);
     const [error, setError] = useState(null);
 
     const source = axios.CancelToken.source();
+    let isAllDataCollected = false;
     const dataCollection = [];
     let pageNumber = 1;
 
+    // Function Calls inside the useEffect
     const fetchData = async(url) => {
         try {
             const response = await axios.get(`${url}&page=${pageNumber}`, {
@@ -19,45 +21,29 @@ function UseAxios(receivedUrl) {
             });
             const APIdata = await response.data;
             if (APIdata.length === 0) {
-                setIsAllDataCollected(true);
-                // console.log("ENTERED")
+                isAllDataCollected = !isAllDataCollected;
             }
-            // console.log(pageNumber);
-            // console.log(APIdata.length);
-            // console.log(isAllDataCollected);
-            // console.log("API - " + APIdata);
-            // console.log("collect before - " + dataCollection)
             dataCollection.push(...APIdata);
-            // console.log("collect after - " + dataCollection)
             setError(null);
         } catch (error) {
             setError(error);
-        } //finally {
-        //     setIsLoading(false);
-        // }
+        }
     }
-    
     const fetchDataHandler = async() => {
         setIsLoading(true)
-        let stopper = 0
-        while (stopper < 8 && isAllDataCollected === false) {
+        while (isAllDataCollected === false) {
             await fetchData(receivedUrl)
             pageNumber++;
-            stopper++;
         } 
-        console.log("stopper at - " + stopper);
-        await setData(dataCollection);
+        setData(dataCollection);
         setIsLoading(false);
     }
-
     const breakDown = () => {
         source.cancel();
     }
 
+    // useEffect
     useEffect(() => {
-        
-        
-        
         // while (error == null && isAllDataCollected === false && pageNumber < 10) { //pageNumber included to safe guard against infinite loops while debugging
         //     fetchData(receivedUrl);
         //     pageNumber++;
@@ -65,13 +51,9 @@ function UseAxios(receivedUrl) {
 
         fetchDataHandler();
         return breakDown;
-    }, [])    
-    console.log({data: data?.[0]?.id, isLoading, isAllDataCollected, error});
+    }, [])   
+
     return {data, isLoading, error}
 }
     
 export default UseAxios;
-
-//TODO -- Update axios to use automatic pagination
-//TODO -- Have started but will come back to it
-//TODO -- See if we can trigger a reRender by using something in the dependencies.
