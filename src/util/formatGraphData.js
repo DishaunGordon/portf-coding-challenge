588a3b4bc3ae919741ad2f2be83a190e607d6c3f
DishaunGordon <dishaun5@gmail.com>
2022-07-25 14:24:35 +0100

    creating a util function to format data

---
+++ src/util/formatGraphData.js
@@ -0,0 +1,17 @@
+export const formatGraphData = (data, startDate, endDate) => {
+
+}
+
+// collect all data
+// check for start date
+// check for end date
+// use date range to generate x-axis
+// iterate over data to get sum of beer in each month
+// each array should look like this to begin with:
+/*
+
+{
+    "month": "MONTH + YEAR", //e.g. Feb 21
+    "totalBrewed": 100,
+}
+*/
