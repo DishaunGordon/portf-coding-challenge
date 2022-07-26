3253e3f9da3bf2067a5227eaa918e42fd6d539ca
DishaunGordon <dishaun5@gmail.com>
2022-07-26 13:23:10 +0100

    Change setup to support React 17 from React 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode> Needed to remove strict mode as in production mode it mounts, unmounts and then mounts again for useEffect
-    <App />
-  // </React.StrictMode>
-);
+const root = document.getElementById("root");
+render(<App />, root);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
