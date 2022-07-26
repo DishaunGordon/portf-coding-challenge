235dfcfda7229f5cd0667f7746977a5d0af0436d
DishaunGordon <dishaun5@gmail.com>
2022-07-26 13:23:45 +0100

    Including nivo Graph

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,16 +1,63 @@
 import React from 'react';
+import { ResponsiveBar } from '@nivo/bar';
+
 function Graph({ data }) {
+
     return ( 
-        <div>
-            <ul>
-                {console.log(data)}
-                {data.map(beer => {
-                    return(
-                    <li key={beer.id}>Name is {beer.name}</li>
-                    )
-                })}
-            </ul>
-        </div>
+        <>
+            <ResponsiveBar
+        data={data}
+        keys={[
+            'totalBrewed'
+        ]}
+        indexBy="month"
+        margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
+        padding={0.3}
+        valueScale={{ type: 'linear' }}
+        indexScale={{ type: 'band', round: true }}
+        colors={{ scheme: 'nivo' }}
+        borderColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    1.6
+                ]
+            ]
+        }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 90,
+            legend: 'Month',
+            legendPosition: 'middle',
+            legendOffset: 45
+        }}
+        axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Total brewed at start of month',
+            legendPosition: 'middle',
+            legendOffset: -40
+        }}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    1.6
+                ]
+            ]
+        }}
+        role="application"
+        ariaLabel="Bar Chart showing number of beers brewed at the start of each month"
+    />
+        </>
      );
 }
 
